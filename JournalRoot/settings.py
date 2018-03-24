5fa246080cb7d64477083ad3df8f66ab139df68d
Alina Shrestha <shralinaiw@gmail.com>
2018-03-24 15:41:51 +0545

    Added requirement file. Used mysql as database

--- JournalRoot/settings.py
+++ JournalRoot/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'JournalRoot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
